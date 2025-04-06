eaaaf4be85344107d5577217771210f74dafd1e3
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-04-06 14:45:46 +0530

    charts component is updated

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,7 +3,7 @@ import { getDashboardData } from "@/actions/dashboard";
 import { getCurrentBudget } from "@/actions/budget";
 import { AccountCard } from "./_components/account-card";
 import { CreateAccountDrawer } from "@/components/create-account-drawer";
-// import { BudgetProgress } from "./_components/budget-progress";
+import { BudgetProgress } from "./_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 // import { DashboardOverview } from "./_components/transaction-overview";
@@ -25,10 +25,10 @@ export default async function DashboardPage() {
   return (
     <div className="space-y-8">
       {/* Budget Progress */}
-      {/* <BudgetProgress
+      <BudgetProgress
         initialBudget={budgetData?.budget}
         currentExpenses={budgetData?.currentExpenses || 0}
-      /> */}
+      />
 
       {/* Dashboard Overview */}
       {/* <DashboardOverview
